454e869ad10971a0312ca34216f328b659f6ba77
Johnathan Moes <johnman747@gmail.com>
2020-04-14 15:25:47 -0500

    added google tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -81,7 +81,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // replace "UA-XXXXXXXXX-X" with your own Tracking ID
-        trackingId: 'UA-XXXXXXXXX-X'
+        trackingId: 'UA-163431103-1'
       }
     }
   ]
